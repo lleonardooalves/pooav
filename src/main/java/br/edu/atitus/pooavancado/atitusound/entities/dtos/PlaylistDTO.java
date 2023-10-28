bb705711c37e775a13d97990e41a3b76c411dc5c
Leonardo Alves <leonardo1029leonardo1029@gmail.com>
2023-10-27 21:15:04 -0300

    v2

---
+++ src/main/java/br/edu/atitus/pooavancado/atitusound/entities/dtos/PlaylistDTO.java
@@ -0,0 +1,16 @@
+package br.edu.atitus.pooavancado.atitusound.entities.dtos;
+
+public class PlaylistDTO {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
